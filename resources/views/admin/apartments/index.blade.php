bed7c26b6722a2f2b00ee466213bf51cf01ae0f5
LeoneCarmelo <carmeloleone@outlook.it>
2023-07-14 22:25:03 +0200

    Index apartment responsive

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -2,15 +2,17 @@
 
 @section('content')
 
-<h1 class="text-center my-3">I MIEI APPARTMAMENTI</h1>
 @include('admin.partials.session_message')
 
-<div class="container-fluid bg-light py-3">
+<div class="container-fluid py-3">
 
-    <a href="{{route('admin.apartments.create')}}"><i class="fa-solid fa-plus fa-2x"></i></a>
-    <table class="table table-striped m-0 py-5">
+    <a href="{{route('admin.apartments.create')}}" class="text-decoration-none text-dark d-flex align-items-center gap-2 py-3">
+        <i class="fa-solid fa-plus fa-2x"></i>
+        <span class="add_apartment">Aggiungi un appartamento</span>
+    </a>
+    <table class="table table-hover m-0 py-5">
         <thead>
-            <tr>
+            <tr class="apartment">
                 <th scope="col">Nome</th>
                 <th scope="col">Immagine</th>
                 <th scope="col">Sponsor</th>
@@ -22,24 +24,24 @@
         <tbody>
 
             @forelse ($apartments as $apartment)
-            <tr>
-                <td class="action-td align-middle" scope="row">{{$apartment->title}}</td>
-                <td class="action-td align-middle">
+            <tr class="apartment">
+                <td class="action-td align-middle" scope="row" data-cell="Title:">{{$apartment->title}}</td>
+                <td class="action-td align-middle" class="img">
                     @if ($apartment->image)
                     <img class="img-fluid" style="height: 100px; width:160px; object-fit:cover;" src=" {{ asset('storage/' . $apartment->image[0]) }}" alt="{{$apartment->slug}}">
                     @else
                     <img class="img-fluid" src=" {{ asset('storage/' . 'uploads/placeholder.png') }}">
                     @endif
                 </td>
-                <td class="action-td text-center align-middle"> @foreach($is_sponsored as $sponsored)
+                <td class="action-td text-center align-middle" data-cell="Sponsor:"> @foreach($is_sponsored as $sponsored)
                     @if($apartment->title == $sponsored->title)
                     si
                     @endif
                     @endforeach
                 </td>
-                <td class="action-td align-middle">{{$apartment->address}}</td>
+                <td class="action-td align-middle" data-cell="Indirizzo:">{{$apartment->address}}</td>
                 {{-- roba da levare l'ho messa giusto per vedere che funziona --}}
-                <td class="action-td text-center align-middle">
+                <td class="action-td text-center align-middle" data-cell="Azioni:">
                     <a class="btn-1 btn-1-blue" href="{{route('admin.apartments.show', $apartment->slug)}}"><i class="fa-solid fa-eye"></i></a>
 
                     <a class="btn-1 btn-1-orange" href="{{route('admin.apartments.edit', $apartment->slug)}}"><i class="fa-solid fa-pencil"></i></a>
