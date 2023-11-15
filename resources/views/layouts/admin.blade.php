631845b89364367c20fb765d2299c6141379b056
giuseppesciacca <sciaccagi@gmail.com>
2023-11-15 19:57:03 +0100

    updpate: version braintree

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -18,7 +18,7 @@
     <script src="https://api.tomtom.com/maps-sdk-for-web/cdn/plugins/SearchBox/3.1.3-public-preview.0/SearchBox-web.js"></script>
     <script src="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.1.2-public-preview.15/services/services-web.min.js"></script>
     <!-- BRAINTREE -->
-    <script src="https://js.braintreegateway.com/web/dropin/1.38.1/js/dropin.min.js"></script>
+    <script src="https://js.braintreegateway.com/web/dropin/1.40.2/js/dropin.js"></script>
     <!-- @vite(['resources/js/braintree.js']) -->
     @vite(['resources/js/app.js'])
 </head>
